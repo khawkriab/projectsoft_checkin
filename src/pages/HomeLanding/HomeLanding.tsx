36991f1e5c739e8a4ad846104d9844531664af0c
khawkriab on mac mini <khawkriab.dev@gmail.com>
2026-04-30 10:43:48 +0700

    update: noti

--- src/pages/HomeLanding/HomeLanding.tsx
+++ src/pages/HomeLanding/HomeLanding.tsx
@@ -1,15 +1,45 @@
-import { Box, Grid } from '@mui/material';
+import { Alert, AlertTitle, Box, Grid, IconButton } from '@mui/material';
 import { TodayCheckIn } from './components/TodayCheckIn';
 import CalendarCheckin from './components/CalendarCheckin';
 import { CheckinHistory } from './components/CheckinHistory';
 import { MenuList } from './components/Menu/MenuList';
 import { UserCalendarProvider } from 'context/UserCalendarProvider';
+import CampaignIcon from '@mui/icons-material/Campaign';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
 
 function HomeLanding() {
+    const [open, setOpen] = useState(true);
+
     return (
         <UserCalendarProvider>
             {/* date */}
             <Box display={{ xs: 'block', lg: 'none' }}>
+                <Alert
+                    slotProps={{
+                        root: { sx: { p: '0 6px', mb: 2 } },
+                    }}
+                    severity='warning'
+                    iconMapping={{
+                        warning: <CampaignIcon fontSize='inherit' />,
+                    }}
+                    action={
+                        <IconButton
+                            aria-label='close'
+                            color='inherit'
+                            size='small'
+                            onClick={() => {
+                                setOpen(false);
+                            }}
+                        >
+                            <CloseIcon fontSize='inherit' />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                    <AlertTitle>ประกาศ!!!!</AlertTitle>
+                    เสื้อบริษัทใหม่ 2 ตัว ให้ใส่วันจันทร์กับวันศุกร์ เริ่ม 4 พ.ค.
+                </Alert>
                 <TodayCheckIn />
             </Box>
             <Grid container spacing={2} direction={{ xs: 'column', lg: 'row' }}>
--- src/pages/HomeLanding/components/Menu/MenuList.tsx
+++ src/pages/HomeLanding/components/Menu/MenuList.tsx
@@ -1,19 +1,48 @@
-import { Box, Grid, Stack, useMediaQuery } from '@mui/material';
+import CampaignIcon from '@mui/icons-material/Campaign';
+import CloseIcon from '@mui/icons-material/Close';
+import { Alert, AlertTitle, Box, Grid, IconButton, Stack, useMediaQuery } from '@mui/material';
+import { useFirebase } from 'context/FirebaseProvider';
+import { useState } from 'react';
+import Holiday from '../HolidayMenuBox';
 import { TodayCheckIn } from '../TodayCheckIn';
 import { LeaveRemainingMenuBox } from './LeaveRemainingMenuBox';
 import { LeaveRequestMenuBox } from './LeaveRequestMenuBox';
-import { SettingsMenuBox } from './SettingsMenuBox';
-import ManageUserCheckinMenuBox from './ManageUserCheckinMenuBox';
-import { useFirebase } from 'context/FirebaseProvider';
 import LogoutMenuBox from './LogoutMenuBox';
-import Holiday from '../HolidayMenuBox';
+import ManageUserCheckinMenuBox from './ManageUserCheckinMenuBox';
+import { SettingsMenuBox } from './SettingsMenuBox';
 import WhoArrivedMenuBox from './WhoArrivedMenuBox';
 
 export function MenuList() {
     const { profile } = useFirebase();
     const desktopSize = useMediaQuery((t) => t.breakpoints.up('lg'));
+    const [open, setOpen] = useState(true);
+
     return (
         <Box>
+            {open && desktopSize && (
+                <Alert
+                    severity='warning'
+                    iconMapping={{
+                        warning: <CampaignIcon fontSize='inherit' />,
+                    }}
+                    action={
+                        <IconButton
+                            aria-label='close'
+                            color='inherit'
+                            size='small'
+                            onClick={() => {
+                                setOpen(false);
+                            }}
+                        >
+                            <CloseIcon fontSize='inherit' />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                    <AlertTitle>ประกาศ!!!!</AlertTitle>
+                    เสื้อบริษัทใหม่ 2 ตัว ให้ใส่วันจันทร์กับวันศุกร์ เริ่ม 4 พ.ค.
+                </Alert>
+            )}
             {desktopSize && <TodayCheckIn />}
             <Stack spacing={1} direction={{ xs: 'column', lg: 'column-reverse' }}>
                 <Grid container spacing={1}>
